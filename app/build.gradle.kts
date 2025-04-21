b24051460f3a593cc13fec72a4be82b7a481e0b8
CRANGGAW\User <crwijaya15@email.com>
2025-04-21 15:43:33 +0700

    Menambahkan fitur untuk mengisi alamat otomatis di halaman Formscreen, mencoba menambahkan review dan ulasan pada Detailscreen menggunakan Firebase tapi gagal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,5 +72,7 @@ dependencies {
     implementation(libs.lottie.compose)
     implementation("com.google.accompanist:accompanist-pager:0.28.0")
     implementation("com.google.accompanist:accompanist-pager-indicators:0.28.0")
-
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
